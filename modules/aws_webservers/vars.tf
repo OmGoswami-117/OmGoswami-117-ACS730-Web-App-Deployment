b0daa4940d3d57f7f9ea131bdc2e0ae8d3f05989
OM GOSWAMI <77207798+OmGoswami-117@users.noreply.github.com>
2024-04-14 20:52:59 -0400

    Update vars.tf

--- modules/aws_webservers/vars.tf
+++ modules/aws_webservers/vars.tf
@@ -1,23 +1,19 @@
-# Default tags
 variable "default_tags" {
   default     = {}
   type        = map(any)
   description = "Default tags to be appliad to all AWS resources"
 }
 
-# Name prefix
 variable "prefix" {
   type        = string
   description = "Name prefix"
 }
 
-# Instance type
 variable "instance_type" {
   description = "Type of the instance"
   type        = string
 }
-
-# Variable to signal the current environment 
+ 
 variable "env" {
   type        = string
   description = "Deployment Environment"
