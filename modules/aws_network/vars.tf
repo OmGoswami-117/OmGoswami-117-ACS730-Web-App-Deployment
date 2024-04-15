e03ebf34ae832c6db46feb9a081a7423edf851b6
OM GOSWAMI <77207798+OmGoswami-117@users.noreply.github.com>
2024-04-14 20:50:18 -0400

    Update vars.tf

--- modules/aws_network/vars.tf
+++ modules/aws_network/vars.tf
@@ -1,23 +1,21 @@
-# Default tags
 variable "default_tags" {
   default     = {}
   type        = map(any)
   description = "Default tags to be appliad to all AWS resources"
 }
 
-# Name prefix
 variable "prefix" {
   type        = string
   description = "Name prefix"
 }
 
-# Provision public subnets in custom VPC
+# custom VPC
 variable "public_cidr_blocks" {
   type        = list(string)
   description = "Public Subnet CIDRs"
 }
 
-# Provision public subnets in custom VPC
+# public subnets in custom VPC
 variable "private_cidr_blocks" {
   type        = list(string)
   description = "Private Subnet CIDRs"
@@ -28,8 +26,7 @@ variable "vpc_cidr" {
   type        = string
   description = "VPC to host Webserver"
 }
-
-# Variable to signal the current environment 
+ 
 variable "env" {
   type        = string
   description = "Deployment Environment"
